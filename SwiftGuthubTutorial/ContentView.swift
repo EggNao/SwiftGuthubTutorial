898376cf225682d28d345352e30f452b11812c19
Naoki Eguchi <eguchi.naoki900@mail.kyutech.jp>
2022-11-10 14:51:48 +0900

    Initial Commit

---
+++ SwiftGuthubTutorial/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftGuthubTutorial
+//
+//  Created by 江口直輝 on 2022/11/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftGuthubTutorial/SwiftGuthubTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGuthubTutorialApp.swift
+//  SwiftGuthubTutorial
+//
+//  Created by 江口直輝 on 2022/11/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGuthubTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
