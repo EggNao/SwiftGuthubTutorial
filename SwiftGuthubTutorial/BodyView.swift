20b24a1e6c5258b926291643a10c2d339ed4cdb8
Naoki Eguchi <eguchi.naoki900@mail.kyutech.jp>
2022-11-24 18:34:25 +0900

    GithubAPIを叩いてデータを取得

--- SwiftGuthubTutorial/BodyView.swift
+++ SwiftGuthubTutorial/BodyView.swift
@@ -7,13 +7,65 @@
 
 import SwiftUI
 
-//func getGithubName(searchText: String) -> Void {
-//    var fetchData = "https://api.github.com/search/repositories?q=\(searchText)"
-//}
+struct GithubRepositoryItems: Codable {
+    let fullName: String
+    let htmlUrl: String
+    let stargazersCount: String
+    let watchersCount: Int
+    enum Key: String, CodingKey {
+            case fullName = "full_name"
+            case htmlUrl = "html_url"
+            case stargazersCount = "stargazers_count"
+            case watchersCount = "watchers_count"
+        }
+}
+
+struct User: Decodable {
+    let id: Int
+    let name: String
+}
+
+class githubRepogitory: ObservableObject {
+    
+    @Published var userData: User?
+    
+    init() {
+        getGithubRepogitory()
+    }
+    
+    func getGithubRepogitory() {
+        // サンプルクエリ
+        let requestURL = URL(string: "https://api.github.com/users/MasakatsuTagishi")!
+        let task = URLSession.shared.dataTask(with: requestURL) { data, response, error in
+            // エラーの時
+            if let error = error {
+                print("error", error)
+            }
+            // jsonが帰ってきた時
+            else if let data = data {
+                let decoder = JSONDecoder()
+                do
+                {
+                    let decoded = try decoder.decode(User.self, from: data)
+                    print(decoded)
+                    DispatchQueue.main.async {
+                        self.userData = decoded
+                    }
+                } catch {
+                    print("error")
+                }
+            }
+        }
+        task.resume()
+        
+    }
+}
 
 
 struct BodyView: View {
     @State var searchText = ""
+    @State var user: User?
+    @ObservedObject var repository = githubRepogitory()
     
     struct GithubName: Identifiable, Hashable {
         var id = UUID()
@@ -30,6 +82,7 @@ struct BodyView: View {
     
     var body: some View {
         VStack {
+            Text(repository.userData?.name ?? "")
             HStack {
                 Image(systemName: "magnifyingglass")
                 TextField("Search ...", text: $searchText)
@@ -47,6 +100,10 @@ struct BodyView: View {
                 }
             }
         }
+        .onAppear(perform:
+                    {
+            print(repository.userData)
+        })
     }
 }
 
--- SwiftGuthubTutorial/ContentView.swift
+++ SwiftGuthubTutorial/ContentView.swift
@@ -15,7 +15,6 @@ struct ContentView: View {
                 Spacer()
             }
             BodyView()
-//            PlagroundView()
         }
         .padding()
     }
